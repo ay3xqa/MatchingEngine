861bb7b5774b2dd6812f33b286b7d8e343f99083
Yu <ay3xqa@virginia.edu>
2023-08-06 23:36:16 -0400

    basically matching engine functionality

--- Engine.py
+++ Engine.py
@@ -8,6 +8,7 @@ class Engine():
         return
     
     #add a sell/buy order to the trading book and return a trade if it matches
+    #TC: O(n*logm) where n is the quantity and m is the size of the heap because popping from heap is logm 
     def submitOrder(self, orderType, price, quantity):
         mapping = {"BUY": self.sellOrders, "SELL": self.buyOrders}
         operations = {"BUY": operator.le, "SELL": operator.ge}
@@ -42,20 +43,36 @@ class Engine():
         return res
     
     #assume is valid to delete
-    #delete a sell/buy order at that price given quantity
-    def deleteOrder(self, orderType, price, quantity):
-        
-        return 
+    #delete a sell/buy order at that price
+    #O(n): n is the size of the heap search to find right element to delete, swap with front and pop, then reheapify 
+    def deleteOrder(self, orderType, price):
+        mapping = {"BUY": self.buyOrders, "SELL": self.sellOrders}
+        heap = mapping[orderType]
+        res = None
+        for idx, order in enumerate(heap):
+            if abs(order.price) == price:
+                heap[0], heap[idx] = heap[idx], heap[0] 
+                res = heapq.heappop(heap)
+                heapq.heapify(heap)
+                break
+        if res:
+            return res
+        return "N/A"
     
     #get total volume at a specified price and ordertype
+    #iterate through heap and sum the quantitys for that price
+    #O(n) where n is size of heap
     def getVolume(self, orderType, price):
-        return
-    
-    #return min and max of orders for a specified order type
-    def getRange(self, orderType):
-        return
+        mapping = {"BUY": self.buyOrders, "SELL": self.sellOrders}
+        heap = mapping[orderType]
+        totalVolume = 0
+        for order in heap:
+            if abs(order.price) == price:
+                totalVolume+=order.quantity
+        return f"Volume: {totalVolume} @ Price: {price}"
     
     def printTradebook(self):
+        print("------------------------------------------")
         print("---BUYS AVAILABLE---")
         for buy in self.buyOrders:
             print(buy)
@@ -84,6 +101,13 @@ e.printTransactions(e.submitOrder("BUY", 3, 150))
 e.printTradebook()
 e.printTransactions(e.submitOrder("BUY", 6, 200))
 e.printTradebook()
-e.printTransactions(e.submitOrder("SELL", 4, 50))
+e.printTransactions(e.submitOrder("BUY", 6, 350))
 e.printTradebook()
+e.printTransactions(e.submitOrder("SELL", 4, 250))
+e.printTradebook()
+e.printTransactions(e.submitOrder("SELL", 9, 50))
+e.printTradebook()
+print("Deleted: "+str(e.deleteOrder("SELL", 9)))
+e.printTradebook()
+print(e.getVolume("BUY", 6))
 # e.submitOrder("SELL", 3, 100)
